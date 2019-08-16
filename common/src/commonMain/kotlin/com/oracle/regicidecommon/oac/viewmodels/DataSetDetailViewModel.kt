4dfad8202ecf67e98e52dbe9234bc51026722cf9
Alex Acosta <SERGIO.ACOSTA@ORACLE.COM>
2019-08-16 15:20:45 -0500

    Added missing view models

---
+++ common/src/commonMain/kotlin/com/oracle/regicidecommon/oac/viewmodels/DataSetDetailViewModel.kt
@@ -0,0 +1,53 @@
+package com.oracle.regicidecommon.oac.viewmodels
+
+import com.oracle.regicidecommon.base.Actions
+import com.oracle.regicidecommon.base.BaseViewModel
+import com.oracle.regicidecommon.base.Coordinator
+import com.oracle.regicidecommon.base.State
+import com.oracle.regicidecommon.coreCommon
+import com.oracle.regicidecommon.models.DataSet
+import com.oracle.regicidecommon.oac.data.OACRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
+import kotlinx.serialization.UnstableDefault
+import org.kodein.di.erased.instance
+
+@ExperimentalCoroutinesApi
+@UnstableDefault
+class DataSetDetailViewModel(val namespace: String, val name: String) :
+    BaseViewModel<Coordinator, DataSetDetailState>(),
+    DataSetDetailActions, Coordinator {
+
+    private val oacRepository: OACRepository by coreCommon.kodein.instance()
+
+    init {
+        launch {
+            oacRepository.fetchDataset(namespace, name)
+                .consumeEach { dataSet ->
+                    stateChannel.mutate { it.copy(dataSet = dataSet) }
+                }
+        }
+    }
+
+    override fun getInitialState() = DataSetDetailState(null, null)
+
+    override fun fetchCanonicalData(namespace: String, name: String) {
+        launch {
+            oacRepository.fetchCanonicalData(namespace, name)
+                .consumeEach { canonicalData ->
+                    stateChannel.mutate { it.copy(canonicalData = canonicalData) }
+                }
+        }
+    }
+
+}
+
+data class DataSetDetailState(
+    val dataSet: DataSet?,
+    val canonicalData: List<List<String>>?
+) : State
+
+interface DataSetDetailActions : Actions {
+    fun fetchCanonicalData(namespace: String, name: String)
+}
